7df7c5a90f749d6dcb52cf725b6b4e4905af30b5
Anton <weymar87@gmail.com>
2022-02-20 16:35:49 +0500

    The first version of my project

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,34 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    int min;
+    int max;
+
+    public Randoms(int min, int max) {
+       this.min = min;
+       this.max = max;
+       this.random = new Random();
+
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            int nextIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                    nextIndex++;
+                    int randomInt = random.nextInt((max - min) + 1) + min;
+                    return randomInt;
+            }
+        };
+    }
+}
